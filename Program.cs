bdc4f377196814a6d720f64ca7fe86bc0371afae
Addison Roy <74374312+mama-sucker@users.noreply.github.com>
2023-09-06 11:26:42 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Module 2 Excercise 2.1.1 
+// Inches to Centimeters Interactive 
+// Addison Roy
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("simple inches to centimters like magic");
+        Console.Write("Enter a number in inches: ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double inches))
+        {
+            double centimeters = inches * 2.54;
+
+            Console.WriteLine($"Inches = {inches}");
+            Console.WriteLine($"Centimeters = {centimeters}");
+        }
+        else
+        {
+            Console.WriteLine("Enter a damn number :)");
+        }
+    }
+}
+
